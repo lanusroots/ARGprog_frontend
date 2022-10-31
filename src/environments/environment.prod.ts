05477bf408d66223beee546afed50d4a9a9f7f55
lanusroots <lanusroots@gmail.com>
2022-10-31 02:11:18 -0300

    Subiendo frontend

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'frontendmf',
+    appId: '1:162296928609:web:9090695a0f0f226f9fcd20',
+    storageBucket: 'frontendmf.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyCACc9HN_5m-UNGezA6wuu8t5VISEo1VSs',
+    authDomain: 'frontendmf.firebaseapp.com',
+    messagingSenderId: '162296928609',
+  },
+  production: true,
+  URL: 'https://backendmf.herokuapp.com/'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'frontendmf',
+    appId: '1:162296928609:web:9090695a0f0f226f9fcd20',
+    storageBucket: 'frontendmf.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyCACc9HN_5m-UNGezA6wuu8t5VISEo1VSs',
+    authDomain: 'frontendmf.firebaseapp.com',
+    messagingSenderId: '162296928609',
+  },
+
+  production: false,
+  URL: 'http://localhost:8080/'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
